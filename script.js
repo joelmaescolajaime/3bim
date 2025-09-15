f4894801ea5b14599e7680b35bd3f9fa9a7c0b2c
joelmaescolajaime <00001127282293SP@al.educacao.sp.gov.br>
2025-09-15 18:33:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -27,13 +27,16 @@ botaoDeAcessibilidade.addEventListener('click', function (){
 
 })
 
+   alternaContraste.addEventListener('click', function(){
+         document.body.classList.toggle('alto-contraste')
+     })
+ 
+ 
+ })
+ 
+ ScrollReveal().reveal('#inicio', { delay: 500 });
+ ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ ScrollReveal().reveal('#galeria', { delay: 500 });
+ ScrollReveal().reveal('#contato', { delay: 500 });
 
-//Código omitido
 
-const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
-botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
-
-//Código omitido
-
-
-<section id="galeria" tabindex="0" aria-label="Seção de galeria de imagens">
